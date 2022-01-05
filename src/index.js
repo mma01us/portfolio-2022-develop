f57ae86d78557a7134c0cbe62111bd0d724c3c70
Michael Ma <mma01us@gmail.com>
2022-01-05 10:50:31 -0500

    Landing page skeleton complete

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.setClearColor(0xE85C47);
+renderer.setClearColor(0x90AACB);
 
 /**
  * Animation GSAP
